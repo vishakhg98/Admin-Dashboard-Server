e5225427b1eacd487783127ca779a2e6ddc59b47
Vishakh <vishakhg98@gmail.com>
2021-09-10 21:41:18 +0530

    cors implemented initial

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const DB_URL = 'mongodb://localhost/Arnowa';
 mongoose.connect(
