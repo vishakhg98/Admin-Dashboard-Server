a662b031ee285d09d26f17ae3f1cadf30b94533d
Vishakh <vishakhg98@gmail.com>
2022-03-06 20:58:36 +0530

    Prettified and added env

--- routes/users.js
+++ routes/users.js
@@ -1,89 +1,89 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Users = require('../model/Users');
+const Users = require("../model/Users");
 
-router.post('/login', async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const userExists = await Users.findOne({ email, password });
-		if (!userExists) {
-			return res.status(403).json({
-				status: false,
-				message: 'Login failed'
-			});
-		}
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExists = await Users.findOne({ email, password });
+    if (!userExists) {
+      return res.status(403).json({
+        status: false,
+        message: "Login failed",
+      });
+    }
 
-		res.status(200).json({
-			status: true,
-			data: userExists._id,
-			message: 'Succesfully logged in'
-		});
-	} catch (err) {
-		res.status(500).json({
-			status: false,
-			message: err.message || 'Something went wrong'
-		});
-	}
+    res.status(200).json({
+      status: true,
+      data: userExists._id,
+      message: "Succesfully logged in",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: err.message || "Something went wrong",
+    });
+  }
 });
 
-router.post('/register', async (req, res) => {
-	try {
-		const { name, email, password } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-		const userExists = await Users.findOne({ email });
+    const userExists = await Users.findOne({ email });
 
-		if (userExists) {
-			return res.status(400).json({
-				status: false,
-				message: 'This user email address is already registered'
-			});
-		}
+    if (userExists) {
+      return res.status(400).json({
+        status: false,
+        message: "This user email address is already registered",
+      });
+    }
 
-		const newUser = await new Users({
-			name,
-			email,
-			password
-		});
-		newUser.save();
-		res.status(200).json({
-			status: true,
-			message: 'Succesfully Registered',
-			data: newUser._id
-		});
-	} catch (err) {
-		res.status(500).json({
-			status: false,
-			message: err.message || 'Something went wrong'
-		});
-	}
+    const newUser = await new Users({
+      name,
+      email,
+      password,
+    });
+    newUser.save();
+    res.status(200).json({
+      status: true,
+      message: "Succesfully Registered",
+      data: newUser._id,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: err.message || "Something went wrong",
+    });
+  }
 });
 
-router.get('/userdata/:id', async (req, res) => {
-	try {
-		const userId = req.params.id;
+router.get("/userdata/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
 
-		const userData = await Users.findOne({ _id: userId });
+    const userData = await Users.findOne({ _id: userId });
 
-		if (!userData) {
-			return res
-				.status(400)
-				.json({ status: false, message: "User doesn't exist", data });
-		}
+    if (!userData) {
+      return res
+        .status(400)
+        .json({ status: false, message: "User doesn't exist", data });
+    }
 
-		const data = userData.toObject();
-		delete data.password;
-		delete data.createdAt;
-		delete data.updatedAt;
+    const data = userData.toObject();
+    delete data.password;
+    delete data.createdAt;
+    delete data.updatedAt;
 
-		res
-			.status(200)
-			.json({ status: true, message: 'User data fetched succesfully', data });
-	} catch (err) {
-		res.status(500).json({
-			status: false,
-			message: err.message || 'Something went wrong'
-		});
-	}
+    res
+      .status(200)
+      .json({ status: true, message: "User data fetched succesfully", data });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: err.message || "Something went wrong",
+    });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// const LOCAL_DB_URL = 'mongodb://localhost/Arnowa';
-const DB_URL =
-	'mongodb+srv://vishakh:vishakhPass@cluster0.sbifl.mongodb.net/cluster0?retryWrites=true&w=majority';
+// const LOCAL_DB_URL = 'mongodb://localhost/Dashboard';
+// mongodb+srv://vishakh:<password>@cluster0.cmybp.mongodb.net/test
+const DB_URL = process.env.DB_URL;
 
 mongoose.connect(
-	DB_URL,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	},
-	() => {
-		console.log('DB connected');
-	}
+  DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB connected");
+  }
 );
 
 app.use(express.json());
-const userRouter = require('./routes/users.js');
-app.use('/', userRouter);
-app.use('/', (req, res) => res.send('YO BACKEND IS WORKING BOI'));
+const userRouter = require("./routes/users.js");
+app.use("/", userRouter);
+app.use("/", (req, res) => res.send("Backed is working"));
 
 const SERVER_PORT = 9000;
 app.listen(process.env.PORT || SERVER_PORT, () =>
-	console.log('Server is running on port:', SERVER_PORT)
+  console.log("Server is running on port:", SERVER_PORT)
 );
